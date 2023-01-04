3d9471399ec149a51f373c77ffa0ee9d3971c2ce
Meelis <mumm.meelis@gmail.com>
2023-01-04 20:39:34 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server running at http://localhost:${PORT}`);
+});
