33b353dc36ad8c288595d3e7ca8269021e1c3d89
Meelis <mumm.meelis@gmail.com>
2023-01-05 17:35:11 +0200

    refactor Router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
-import routes from './routes.js';
+import router from './router.js';
 
 import connectDatabase from '../utils/db.js';
 
@@ -9,9 +9,9 @@ dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT;
 
+app.use('/api', router);
+
 app.listen(PORT, async () => {
     console.log(`[server]: Server running at http://localhost:${PORT}`);
     await connectDatabase();
-
-    routes(app);
 });
--- src/routes.ts
+++
@@ -1,7 +0,0 @@
-import { Express, Request, Response } from 'express';
-
-function routes(app: Express) {
-    app.get('/ping', (req: Request, res: Response) => res.send('pong'));
-}
-
-export default routes;
