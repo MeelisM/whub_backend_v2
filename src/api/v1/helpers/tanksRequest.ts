5a3a398e43feda06cc6a179aff7a45dbbd1e46ca
Meelis <mumm.meelis@gmail.com>
2023-01-08 13:28:20 +0200

    tanksRequests properly throws an error

--- src/api/v1/helpers/tanksRequest.ts
+++ src/api/v1/helpers/tanksRequest.ts
@@ -5,16 +5,16 @@ export async function getWargamingData() {
     const API_KEY = process.env.API_KEY as string;
     const requestUrl: string = `https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=${API_KEY}&fields=-radios%2C-tag%2C-suspensions%2C-provisions%2C-engines%2C-crew%2C-guns%2C-multination%2C-description%2C-is_premium_igr%2C-next_tanks%2C-modules_tree%2C-prices_xp%2C-price_gold%2C-price_credit%2C-default_profile%2C-turrets%2C-name%2C-images.small_icon%2C-images.big_icon`;
     const response = await fetch(requestUrl);
-
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
     const data: IResponseDataWargaming = await response.json();
-    console.log('[RESPONSE]: Success! Tanks list recieved');
+
+    if (data.status === 'error') {
+        throw new Error(data.error?.message);
+    }
     return data;
 }
 
 interface IResponseDataWargaming {
+    error?: IResponseDataWargamingErrorMessage;
     status: string;
     meta: {
         count: number;
@@ -40,4 +40,11 @@ interface ISingleTankWargaming {
 
 interface ISingleTankImage {
     contour_icon: string;
+}
+
+interface IResponseDataWargamingErrorMessage {
+    field: string;
+    message: string;
+    code: number;
+    value: string;
 }
