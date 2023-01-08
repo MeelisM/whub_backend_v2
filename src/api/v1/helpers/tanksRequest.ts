94ccfd73eca13dcb3f984645776eab1800e7fa3f
Meelis <mumm.meelis@gmail.com>
2023-01-08 11:37:27 +0200

    refactoring

--- src/api/v1/helpers/tanksRequest.ts
+++ src/api/v1/helpers/tanksRequest.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export async function getWargamingData() {
-    const API_KEY = process.env.API_KEY;
+    const API_KEY = process.env.API_KEY as string;
     const requestUrl: string = `https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=${API_KEY}&fields=-radios%2C-tag%2C-suspensions%2C-provisions%2C-engines%2C-crew%2C-guns%2C-multination%2C-description%2C-is_premium_igr%2C-next_tanks%2C-modules_tree%2C-prices_xp%2C-price_gold%2C-price_credit%2C-default_profile%2C-turrets%2C-name%2C-images.small_icon%2C-images.big_icon`;
     const response = await fetch(requestUrl);
 
