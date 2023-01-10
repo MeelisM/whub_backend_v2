1a0ea16a81454f4d4d909edff54647f8bb04ce68
Meelis <mumm.meelis@gmail.com>
2023-01-10 13:07:02 +0200

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
+import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import router from './router.js';
 import connectDatabase from './config/db.js';
@@ -7,12 +8,18 @@ import connectDatabase from './config/db.js';
 dotenv.config();
 
 const app: Express = express();
+
+// global variables
 const PORT = process.env.PORT;
 
+// routes
 app.use('/api', router);
+
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.listen(PORT, async () => {
     console.log(`[SERVER]: Server running at http://localhost:${PORT}`);
